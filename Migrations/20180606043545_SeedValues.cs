67a881c92ab4d3e4a78da85b5c2687f6d30d7900
Daniel <drscarbro@L-910012.arvnet.ad.arvest.com>
2018-06-06 00:23:53 -0500

    Initial commit

---
+++ Migrations/20180606043545_SeedValues.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class SeedValues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Values", "Name", "Value 101");
+            migrationBuilder.InsertData("Values", "Name", "Value 102");
+            migrationBuilder.InsertData("Values", "Name", "Value 103");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
